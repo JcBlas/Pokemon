350890c266a63cc2224af5e2004694d8fbc9fa2c
JcBlas <johnusblas@gmail.com>
2023-12-05 18:54:52 +0800

    lagay

---
+++ projectsite/cardquest/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import PokemonCard, Trainer
+# Register your models here.
+@admin.register(PokemonCard)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("name", "rarity")
+    search_fields = ("name",)
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = ("name", "location", "email")
+    search_fields = ("name",)
