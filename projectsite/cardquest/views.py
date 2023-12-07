de4df86eca5a51076d5eadae72b2e292926ca7b7
JcBlas <johnusblas@gmail.com>
2023-12-07 21:31:39 +0800

    add urlpy

--- projectsite/cardquest/views.py
+++ projectsite/cardquest/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.list import ListView
+from cardqquest.models import PokemonCard
+
+class HomePageView(ListView):
+    model = PokemonCard
+    context_object_name = 'home'
+    template_name = "home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
